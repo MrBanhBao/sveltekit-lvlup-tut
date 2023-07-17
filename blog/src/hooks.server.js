93d0bf3812f7e031b5e6e5588c2c81ef2fa0d235
Hao Nguyen <heo.hao@gmail.com>
2023-07-17 16:38:49 +0200

    sequayence hook and logger

--- blog/src/hooks.server.js
+++ blog/src/hooks.server.js
@@ -1,9 +1,35 @@
+import { sequence } from "@sveltejs/kit/hooks";
 import { auth } from "$db/fake_auth"
 
-export async function handle({ event, resolve }) {
+async function logger({ event, resolve }) {
+    const start_time = Date.now();
+    console.log('start_time', start_time);
+    
+    const response = await resolve(event);
+
+    console.log(`${Date.now() - start_time}ms - ${event.request.method} - ${event.url.pathname}`);
+
+    return response;
+}
+
+function authorize({ event, resolve }) {
     const user = auth();
+    console.log('hi');
     event.locals.user = user;
-    event.locals.this_is_test = 'Test';
-    console.log('event', event)
     return resolve(event)
+}
+
+
+export const handle = sequence(logger, authorize);
+
+export function handleFetch({request, fetch}) {
+    // intercepting fetch and tweak request and fetch it at
+    return fetch(request);
+}
+
+export function handleError({error, event}) {
+    return {
+        message: 'Oops, I#m intercepting in a hook',
+        code: error.code
+    }
 }
