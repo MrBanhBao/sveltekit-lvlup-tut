068e365bca279582faeeb7bddc3b3196ea7dc59d
Hao Nguyen <heo.hao@gmail.com>
2023-07-20 08:44:39 +0200

    sveltekit stores

--- blog/src/routes/+page.js
+++ blog/src/routes/+page.js
@@ -4,7 +4,7 @@ export async function load({ fetch, parent }) {
 
     const res = await fetch('https://syntax.fm/api/shows/latest');
     const data = await res.json();
-    console.log(data);
+    //console.log(data);
     return {
         latest_episode: data,
     }
